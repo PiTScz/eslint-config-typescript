21d82bc9164414cfad8710929651394e0a362f3f
Radek Hřebeček <radek@hrebecek.cz>
2023-12-01 11:54:52 +0100

    feat: create initial project (#1)

---
+++ .eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: [
+    'next',
+    'next/core-web-vitals',
+    'plugin:react/recommended',
+    'plugin:@next/next/recommended',
+    'plugin:sort/recommended',
+    'plugin:typescript-sort-keys/recommended',
+    'plugin:prettier/recommended',
+    'plugin:testing-library/react',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    'react-hooks',
+    '@typescript-eslint',
+    'sort',
+    'prettier',
+    'testing-library',
+  ],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'testing-library/no-node-access': 'off',
+    'testing-library/no-render-in-lifecycle': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
